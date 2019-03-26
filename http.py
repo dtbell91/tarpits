f5e47ef5d8ad0da8498b29c49596e1d730e7a1b1
David Bell <dtbell91@gmail.com>
2019-03-26 17:48:03 +1100

    add max connection limit, logging; improve perf

--- http.py
+++ http.py
@@ -3,24 +3,36 @@
 import asyncio
 import random
 import logging
+from datetime import datetime
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s : %(levelname)s : %(message)s')
 
 async def handler(_reader, writer):
-    logging.info('status="new connection" client_ip="%s:%s"' % (writer.get_extra_info('peername')))
+    client = '%s:%s' % writer.get_extra_info('peername')
+    connections = len(asyncio.all_tasks())
+    if connections > 500:
+        logging.info('status="too many connections" client="%s" connections=%d' % (client, connections))
+        await writer.drain()
+        writer.close()
+        return
+
+    start = datetime.now()
+    logging.info('status="new connection" client="%s" connections=%d' % (client, connections))
     writer.write(b'HTTP/1.1 200 OK\r\n')
     try:
         while True:
-            await asyncio.sleep(5)
-            header = random.randint(0, 2**32)
-            value = random.randint(0, 2**32)
-            logging.info('status="sending random data" client_ip="%s:%s"' % (writer.get_extra_info('peername')))
+            wait = 10
+            await asyncio.sleep(wait)
+            header = random.getrandbits(32)
+            value = random.getrandbits(32)
             writer.write(b'X-%x: %x\r\n' % (header, value))
             await writer.drain()
     except ConnectionResetError:
-        logging.info('status="connection reset" client_ip="%s:%s"' % (writer.get_extra_info('peername')))
+        end = datetime.now()
+        duration = end - start
+        logging.info('status="connection reset" client="%s" duration="%s" connections=%d' % (client, duration, connections))
         pass
 
 async def main():
